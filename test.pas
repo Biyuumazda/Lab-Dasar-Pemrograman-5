8be33e11ec99ab51ba4867da939efdfbe410adc9
Biyuumazda <wildanabiyumazda@gmail.com>
2026-09-07 20:19:13 +0700

    Mencoba Set Up Github dan Runnning Pascal di VSC

---
+++ test.pas
@@ -0,0 +1,15 @@
+{
+Nama : Wildan Abiyu Mazda
+NIM : 261401063
+Kelas : KOM-C
+}
+
+program Soal6;
+var
+    angka : integer;
+begin
+    write('Masukkan angka: ');
+    readln(angka);
+
+writeln ('Angka yang Anda masukkan: ', angka);
+end.
